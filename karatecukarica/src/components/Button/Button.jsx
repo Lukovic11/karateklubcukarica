c90fcf0a8ec44594ad5f2712eae189ddfaf116b6
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2024-10-26 17:57:53 +0200

    button done

---
+++ karatecukarica/src/components/Button/Button.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+
+const Button = (props) => {
+  return (
+    <motion.button
+      to={props.linkURL}
+      className={`button${props.variation}`}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      {props.text}
+    </motion.button>
+  );
+};
+Button.propTypes = {
+  linkURL: PropTypes.string.isRequired,
+  variation: PropTypes.string,
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
