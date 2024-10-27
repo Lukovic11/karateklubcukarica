6eca05aa3e0e85628f4d012c7c53754da3e2edb4
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2024-10-27 11:45:46 +0100

    started header

--- karatecukarica/src/pages/Root.jsx
+++ karatecukarica/src/pages/Root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
 
 const Root = () => {
-    return ( 
-        <div className="container">
-            <main>
-                <Outlet />
-            </main>
-        </div>
-     );
-}
- 
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 export default Root;
