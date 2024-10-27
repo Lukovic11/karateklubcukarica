089a5a3b86adc77b01b867ee73913e5e854a9ab3
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2024-10-27 12:01:07 +0100

    extracted text from all of the components

--- karatecukarica/src/components/Herobanner/Herobanner.jsx
+++ karatecukarica/src/components/Herobanner/Herobanner.jsx
@@ -2,21 +2,32 @@ import styles from "./Herobanner.module.scss";
 import Button from "../Button/Button";
 import herobanner_img from "../../assets/herobanner_img.jpg";
 // import logo from "../../assets/logo.png";
+import {
+  SCHEDULES_AND_LOCATIONS_URL,
+  HEROBANNER_IMG_ALT,
+  HEROBANNER_TEXT_1,
+  HEROBANNER_TEXT_2,
+  HEROBANNER_BUTTON_TEXT,
+} from "../../constants";
 
 //TODO: Change herobanner image and alt text
 const HeroBanner = () => {
   return (
     <div className={styles.herobanner_container}>
       <div className={styles.herobanner}>
-        <img src={herobanner_img} alt="Herobanner image" />
+        <img src={herobanner_img} alt={HEROBANNER_IMG_ALT} />
         <div className={styles.overlay}>
           <div className={styles.content}>
             {/* TODO: Decide if we want to use logo or not */}
             {/* <img src={logo} alt="Logo" /> */}
             <h1>
-              KARATE KLUB <div>ČUKARICA</div>
+              {HEROBANNER_TEXT_1} <div>{HEROBANNER_TEXT_2}</div>
             </h1>
-            <Button linkURL="/terminiilokacije" text="UPIŠI SE" variation="" />
+            <Button
+              linkURL={`/${SCHEDULES_AND_LOCATIONS_URL}`}
+              text={HEROBANNER_BUTTON_TEXT}
+              variation=""
+            />
           </div>
         </div>
       </div>
---
+++ karatecukarica/src/constants.js
@@ -0,0 +1,15 @@
+//TestComponent.jsx
+export const H2_TEXT = "KARATE KLUB";
+export const H1_TEXT = "ČUKARICA";
+export const P_TEXT =
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore dolorem vitae tenetur rerum, ratione velit. Facilis rem commodi quasi ullam, perferendis recusandae. Ab unde rem quia pariatur hic tempora accusantium vitae modi tempore totam laboriosam deleniti debitis asperiores, minus ex temporibus cupiditate, fugiat sunt corporis nam non beatae, perspiciatis dolorem magnam. Adipisci reiciendis quibusdam laboriosam amet repudiandae facere iure tenetur esse, cumque, ipsum ullam culpa eligendi voluptatibus, quae earum quidem fugiat asperiores? Obcaecati aut ducimus ad. Fugit, ullam officiis natus at sunt reprehenderit doloribus. Sunt quo possimus voluptatem fuga voluptate, harum sed deserunt error obcaecati pariatur animi odit eaque eum impedit assumenda numquam nesciunt unde tempora enim perferendis ut iste! Dolorem officia cum optio autem iste iure, sequi a eius vel est assumenda error quo illum veniam cupiditate itaque necessitatibus ipsam qui hic eaque ducimus? Quod atque minus unde excepturi quam facere, modi autem perspiciatis, harum numquam nostrum placeat consequuntur porro magni nulla eum sint facilis cupiditate ex doloremque obcaecati vel ducimus itaque. Natus consequatur hic repellendus magni nihil optio possimus recusandae neque, voluptatum sunt atque obcaecati culpa ullam ipsam, nisi quae non eum fugit ducimus nesciunt debitis beatae similique quidem asperiores. Maxime, distinctio ut molestias repellat aspernatur vero optio quia, sapiente totam sequi aliquid? Corporis hic accusamus facere odit repudiandae earum, dolor mollitia placeat non amet maiores obcaecati pariatur necessitatibus nam quaerat recusandae aut ipsam ratione vel eum. Sapiente dolores necessitatibus veniam repudiandae at ";
+//App.jsx
+export const SCHEDULES_AND_LOCATIONS_URL = "/terminiilokacije";
+export const SUMMER_CAMP_URL = "/letnjikamp";
+//Herobanner.jsx
+export const HEROBANNER_IMG_ALT = "Herobanner image";
+export const HEROBANNER_TEXT_1 = "KARATE KLUB";
+export const HEROBANNER_TEXT_2 = "ČUKARICA";
+export const HEROBANNER_BUTTON_TEXT = "UPIŠI SE";
+//BUTTON VARIATIONS
+export const BUTTON_VARIATION_TRANSPARENT = "__transparent";
