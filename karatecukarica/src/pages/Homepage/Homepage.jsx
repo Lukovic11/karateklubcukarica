1184561150a3a4f273bc9bbd92741f9cf00b03a4
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2024-11-05 15:47:06 +0100

    bug fix

--- karatecukarica/src/pages/Homepage/Homepage.jsx
+++ karatecukarica/src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,20 @@
 import HeroBanner from "../../components/Herobanner/Herobanner";
+import Split from "../../components/Split/Split";
+import { SPLIT_TITLE } from "../../constants";
 
 const Homepage = () => {
   return (
     <div>
       <HeroBanner />
+      <Split
+        orientation="image-right"
+        buttonProps={{
+          text: "Button",
+          variation: "__transparent",
+          linkURL: "/letnjikamp",
+        }}
+        title={SPLIT_TITLE}
+      />
     </div>
   );
 };
