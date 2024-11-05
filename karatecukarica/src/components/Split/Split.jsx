73114b935a84424943abc5a15bb1f260b721a25a
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2024-11-05 15:33:02 +0100

    split component done

--- karatecukarica/src/components/Split/Split.jsx
+++ karatecukarica/src/components/Split/Split.jsx
@@ -1,15 +1,15 @@
 import splitImage from "../../assets/split-img.jpg";
-import { SPLIT_IMG_ALT, SPLIT_TITLE, SPLIT_TEXT } from "../../constants";
+import { SPLIT_IMG_ALT, SPLIT_TEXT } from "../../constants";
 import Button from "../Button/Button";
 import "./Split.scss";
 import PropTypes from "prop-types";
 
-const Split = ({ orientation, buttonProps }) => {
+const Split = ({ orientation, buttonProps, title }) => {
   return (
     <div className={`split-container ${orientation ? orientation : ""}`}>
       <img src={splitImage} alt={SPLIT_IMG_ALT} />
       <div className="inner-content">
-        <h3>{SPLIT_TITLE}</h3>
+        {title && <h3>{title}</h3>}
         <p>{SPLIT_TEXT}</p>
         {buttonProps && <Button className="button-split" {...buttonProps} />}
       </div>
@@ -19,6 +19,7 @@ const Split = ({ orientation, buttonProps }) => {
 
 Split.propTypes = {
   orientation: PropTypes.string,
+  buttonProps: PropTypes.object,
 };
 
 export default Split;
--- karatecukarica/src/pages/Homepage/Homepage.jsx
+++ karatecukarica/src/pages/Homepage/Homepage.jsx
@@ -1,17 +1,9 @@
 import HeroBanner from "../../components/Herobanner/Herobanner";
-import Split from "../../components/Split/Split";
 
 const Homepage = () => {
   return (
     <div>
       <HeroBanner />
-      <Split
-        buttonProps={{
-          text: "Button",
-          variation: "__transparent",
-          linkURL: "/letnjikamp",
-        }}
-      />
     </div>
   );
 };
