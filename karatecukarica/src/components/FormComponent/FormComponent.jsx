25ed540ee828ce7d48230dd68751fe1485e55951
lenalonc <115535945+lenalonc@users.noreply.github.com>
2025-01-13 19:15:41 +0100

    Feature 13/form component (#29)
    * Mobile view test 1
    
    * za ivanu da pogleda
    
    * 'ups'
    
    * ip
    
    * scss changes
    
    * scss changes
    
    * Form component mobile view
    
    * Form component responsive
    
    * small fix
    
    * small
    
    * additional changes
    
    * css change
    
    * Validation form form component
    
    * Additional changes related to the comments on PR
    
    * Minor changes
    
    * nesto
    
    ---------
    
    Co-authored-by: lukovic11 <115374310+Lukovic11@users.noreply.github.com>

---
+++ karatecukarica/src/components/FormComponent/FormComponent.jsx
@@ -0,0 +1,163 @@
+import { useState } from "react";
+import {
+  FORM_FIRSTNAME,
+  FORM_LASTNAME,
+  FORM_EMAIL,
+  FORM_SUBJECT,
+  FORM_QUESTION,
+  FORM_SEND,
+  FIRSTNAME_NUMBER_ERROR,
+  FIRSTNAME_EMPTY_ERROR,
+  LASTNAME_NUMBER_ERROR,
+  LASTNAME_EMPTY_ERROR,
+  SUBJECT_EMPTY_ERROR,
+  QUESTION_EMPTY_ERROR,
+} from "../../constants";
+import Button from "../Button/Button";
+import { motion } from "framer-motion";
+import "./FormComponent.scss";
+import PropTypes from "prop-types";
+
+const FormComponent = ({ handleSubmit }) => {
+  const [focusedField, setFocusedField] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const [formData, setFormData] = useState({
+    [FORM_FIRSTNAME]: "",
+    [FORM_LASTNAME]: "",
+    [FORM_EMAIL]: "",
+    [FORM_SUBJECT]: "",
+    [FORM_QUESTION]: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const validateForm = () => {
+    let formIsValid = true;
+    let currentErrors = {};
+
+    Object.entries(formData).forEach(([key, value]) => {
+      const trimmedValue = value.trim();
+      let error = "";
+      let error_type = "";
+
+      if (key === FORM_FIRSTNAME) {
+        if (/\d/.test(trimmedValue)) {
+          error = FIRSTNAME_NUMBER_ERROR;
+          error_type = key;
+        } else if (trimmedValue === "") {
+          error = FIRSTNAME_EMPTY_ERROR;
+          error_type = key;
+        }
+      } else if (key === FORM_LASTNAME) {
+        if (/\d/.test(trimmedValue)) {
+          error = LASTNAME_NUMBER_ERROR;
+          error_type = key;
+        } else if (trimmedValue === "") {
+          error = LASTNAME_EMPTY_ERROR;
+          error_type = key;
+        }
+      } else if (key === FORM_SUBJECT) {
+        if (trimmedValue === "") {
+          error = SUBJECT_EMPTY_ERROR;
+          error_type = key;
+        }
+      } else if (key === FORM_QUESTION) {
+        if (trimmedValue === "") {
+          error = QUESTION_EMPTY_ERROR;
+          error_type = key;
+        }
+      }
+
+      if (error) {
+        currentErrors[key] = error;
+        formIsValid = false;
+      }
+    });
+
+    setErrors(currentErrors);
+    return formIsValid;
+  };
+
+  const fields = [
+    { label: FORM_FIRSTNAME, name: FORM_FIRSTNAME, type: "text" },
+    { label: FORM_LASTNAME, name: FORM_LASTNAME, type: "text" },
+    { label: FORM_EMAIL, name: FORM_EMAIL, type: "email" },
+    { label: FORM_SUBJECT, name: FORM_SUBJECT, type: "text" },
+    { label: FORM_QUESTION, name: FORM_QUESTION, type: "textarea" },
+  ];
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validateForm();
+    if (isValid) {
+      handleSubmit(formData);
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <form className="form-contact" onSubmit={onSubmit} autoComplete="off">
+        {fields.map(({ label, name, type }) => (
+          <motion.div className="form-field" key={name}>
+            <label className="form-label">{label}:</label>
+            {type === "textarea" ? (
+              <textarea
+                name={name}
+                value={formData[name]}
+                onChange={handleChange}
+                required
+                className="form-textarea"
+                autoComplete="off"
+              />
+            ) : (
+              <motion.input
+                type={type}
+                name={name}
+                value={formData[name]}
+                onChange={handleChange}
+                required
+                className="form-input"
+                onFocus={() => setFocusedField(name)}
+                onBlur={() => setFocusedField("")}
+                animate={{
+                  borderBottomColor:
+                    focusedField === name ? "#b20000" : "#f9f9f9",
+                }}
+                transition={{
+                  duration: 0.4,
+                  ease: "circInOut",
+                }}
+              />
+            )}
+
+            {errors[name] && (
+              <motion.span
+                className="form-error"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              >
+                {errors[name]}
+              </motion.span>
+            )}
+          </motion.div>
+        ))}
+        <Button linkURL={"/"} variation={""} text={FORM_SEND} />
+      </form>
+    </div>
+  );
+};
+
+FormComponent.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default FormComponent;
--- karatecukarica/src/constants.js
+++ karatecukarica/src/constants.js
@@ -2,7 +2,7 @@
 export const H2_TEXT = "KARATE KLUB";
 export const H1_TEXT = "ČUKARICA";
 export const P_TEXT =
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore dolorem vitae tenetur rerum, ratione velit. Facilis rem commodi quasi ullam, perferendis recusandae. Ab unde rem quia pariatur hic tempora accusantium vitae modi tempore totam laboriosam deleniti debitis asperiores, minus ex temporibus cupiditate, fugiat sunt corporis nam non beatae, perspiciatis dolorem magnam. Adipisci reiciendis quibusdam laboriosam amet repudiandae facere iure tenetur esse, cumque, ipsum ullam culpa eligendi voluptatibus, quae earum quidem fugiat asperiores? Obcaecati aut ducimus ad. Fugit, ullam officiis natus at sunt reprehenderit doloribus. Sunt quo possimus voluptatem fuga voluptate, harum sed deserunt error obcaecati pariatur animi odit eaque eum impedit assumenda numquam nesciunt unde tempora enim perferendis ut iste! Dolorem officia cum optio autem iste iure, sequi a eius vel est assumenda error quo illum veniam cupiditate itaque necessitatibus ipsam qui hic eaque ducimus? Quod atque minus unde excepturi quam facere, modi autem perspiciatis, harum numquam nostrum placeat consequuntur porro magni nulla eum sint facilis cupiditate ex doloremque obcaecati vel ducimus itaque. Natus consequatur hic repellendus magni nihil optio possimus recusandae neque, voluptatum sunt atque obcaecati culpa ullam ipsam, nisi quae non eum fugit ducimus nesciunt debitis beatae similique quidem asperiores. Maxime, distinctio ut molestias repellat aspernatur vero optio quia, sapiente totam sequi aliquid? Corporis hic accusamus facere odit repudiandae earum, dolor mollitia placeat non amet maiores obcaecati pariatur necessitatibus nam quaerat recusandae aut ipsam ratione vel eum. Sapiente dolores necessitatibus veniam repudiandae at ";
+"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore dolorem vitae tenetur rerum, ratione velit. Facilis rem commodi quasi ullam, perferendis recusandae. Ab unde rem quia pariatur hic tempora accusantium vitae modi tempore totam laboriosam deleniti debitis asperiores, minus ex temporibus cupiditate, fugiat sunt corporis nam non beatae, perspiciatis dolorem magnam. Adipisci reiciendis quibusdam laboriosam amet repudiandae facere iure tenetur esse, cumque, ipsum ullam culpa eligendi voluptatibus, quae earum quidem fugiat asperiores? Obcaecati aut ducimus ad. Fugit, ullam officiis natus at sunt reprehenderit doloribus. Sunt quo possimus voluptatem fuga voluptate, harum sed deserunt error obcaecati pariatur animi odit eaque eum impedit assumenda numquam nesciunt unde tempora enim perferendis ut iste! Dolorem officia cum optio autem iste iure, sequi a eius vel est assumenda error quo illum veniam cupiditate itaque necessitatibus ipsam qui hic eaque ducimus? Quod atque minus unde excepturi quam facere, modi autem perspiciatis, harum numquam nostrum placeat consequuntur porro magni nulla eum sint facilis cupiditate ex doloremque obcaecati vel ducimus itaque. Natus consequatur hic repellendus magni nihil optio possimus recusandae neque, voluptatum sunt atque obcaecati culpa ullam ipsam, nisi quae non eum fugit ducimus nesciunt debitis beatae similique quidem asperiores. Maxime, distinctio ut molestias repellat aspernatur vero optio quia, sapiente totam sequi aliquid? Corporis hic accusamus facere odit repudiandae earum, dolor mollitia placeat non amet maiores obcaecati pariatur necessitatibus nam quaerat recusandae aut ipsam ratione vel eum. Sapiente dolores necessitatibus veniam repudiandae at ";
 //App.jsx
 export const SCHEDULES_AND_LOCATIONS_URL = "/terminiilokacije";
 export const SUMMER_CAMP_URL = "/letnjikamp";
@@ -20,6 +20,19 @@ export const HEADER_PAGE_LIST = [
   "Uslovi",
   "Kontakt",
 ];
+//Form.jsx
+export const FORM_FIRSTNAME = "Ime";
+export const FORM_LASTNAME = "Prezime";
+export const FORM_EMAIL = "Email";
+export const FORM_SUBJECT = "Predmet";
+export const FORM_QUESTION = "Pitanje";
+export const FORM_SEND = "Pošalji";
+export const FIRSTNAME_NUMBER_ERROR = 'Ime ne sme da sadrži brojeve';
+export const LASTNAME_NUMBER_ERROR = 'Prezime ne sme da sadrži brojeve';
+export const FIRSTNAME_EMPTY_ERROR = 'Ime ne sme da bude prazno';
+export const LASTNAME_EMPTY_ERROR = 'Prezime ne sme da bude prazno';
+export const SUBJECT_EMPTY_ERROR = 'Predmet ne sme da bude prazan';
+export const QUESTION_EMPTY_ERROR = 'Pitanje ne sme da bude prazno';
 //Split.jsx
 export const SPLIT_IMG_ALT = "Image for split component";
 export const SPLIT_TITLE = "Title";
