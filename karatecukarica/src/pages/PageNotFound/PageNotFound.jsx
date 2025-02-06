d7e73df738e38ca89ce76d9dfc960bcd6a22953c
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-02-06 15:45:14 +0100

    fix

--- karatecukarica/src/pages/PageNotFound/PageNotFound.jsx
+++ karatecukarica/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,14 +1,15 @@
 import "./PageNotFound.scss"
 import karatedo from "../../assets/karatedo.svg?react"
 import {Link} from "react-router-dom";
+import {PNF_IMG_ALT, PNF_TEXT, PNF_TITLE} from "../../constants.jsx";
 
 const PageNotFound = () => {
   return (
     <div className="pagenotfound-wrapper">
-      <img className="background-img" src={karatedo} alt="Karate Do in Japanese"/>
+      <img className="background-img" src={karatedo} alt={PNF_IMG_ALT}/>
       <div className="text">
-        <h1>Tražena stranica ne postoji</h1>
-        <Link to="/">Vrati se na početnu stranicu</Link>
+        <h1>{PNF_TITLE}</h1>
+        <Link to="/">{PNF_TEXT}</Link>
       </div>
     </div>
   );
