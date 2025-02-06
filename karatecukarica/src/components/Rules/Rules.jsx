22e2f05e87501f8815c77a4bcb6dd3e1044ba887
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-02-06 16:06:54 +0100

    fix

--- karatecukarica/src/components/Rules/Rules.jsx
+++ karatecukarica/src/components/Rules/Rules.jsx
@@ -1,5 +1,15 @@
 import {Accordion, AccordionHeader, AccordionItem, AccordionPanel,} from "../Accordion/Accordion.jsx"
 import {Plus} from 'lucide-react';
+import {
+  ITEM1_TEXT,
+  ITEM1_TITLE,
+  ITEM2_TEXT,
+  ITEM3_TEXT,
+  ITEM4_TEXT,
+  ITEM5_TEXT,
+  ITEM6_TEXT,
+  ITEM7_TEXT
+} from "../../constants";
 
 function Rules() {
   return (
@@ -8,77 +18,66 @@ function Rules() {
       <Accordion>
         <AccordionItem value="item-1">
           <AccordionHeader icon={<Plus/>}>
-            Oprema za trening
+            {ITEM1_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Da bi dete počelo da vežba karate, kimono nije neophodan. Dovoljni su trenerka dugih nogavica i majica
-            kratkih rukava.
+            {ITEM1_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-2">
           <AccordionHeader icon={<Plus/>}>
-            Osvežavajući napici
+            {ITEM2_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Jedini napitak koji može da hidrira dete je VODA i zato nije poželjno donositi sokove i slične napitke.
-            Potrebno je da dete ima svoju PLASTIČNU flašicu, obeleženu tako da može da je prepozna.
+            {ITEM2_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionHeader icon={<Plus/>}>
-            Priprema za trening i presvlačenje
+            {ITEM3_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Bez obzira koliko godina deca imaju, potrebno je da se uče samostalnosti i odgovornosti. To je jedan od
-            zadataka i ciljeva treninga. Zato je važno dovesti dete na vreme (desetak minuta pre treninga) i pustiti ga
-            da se samostalno presvuče.
+            {ITEM3_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-4">
           <AccordionHeader icon={<Plus/>}>
-            Higijena
+            {ITEM4_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Deca se uče da održavaju ličnu higijenu, ali i da vode računa o čistoći prostora koji koriste i u kojem
-            borave. To je naša zajednička odgovornost. U sali i svlačionici deca hodaju bosa. Iz tog razloga, samo
-            vežbači mogu da koriste ovaj prostor i o njemu vode računa.
+            {ITEM4_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-5">
           <AccordionHeader icon={<Plus/>}>
-            Prisustvo roditelja na treningu
+            {ITEM5_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Roditeljima nije dozvoljen ulazak u salu. Trening je vreme Vašeg deteta i čini deo njegove privatnosti. Osim
-            toga, Vaše prisustvo bi remetilo detetovu pažnju i otežavalo posao trenera.
+            {ITEM5_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-6">
           <AccordionHeader icon={<Plus/>}>
-            Komunikacija trenera i roditelja
+            {ITEM6_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Molimo Vas za strpljenje. U vreme treninga, trener ne može komunicirati sa roditeljima, ali to može učiniti
-            pre ili nakon treninga.
+            {ITEM6_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-7">
           <AccordionHeader icon={<Plus/>}>
-            Komunikacija sa sekretarom kluba
+            {ITEM7_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Sekretar kluba dostupan Vam je pre ili posle treninga u prostorijama kluba, ali će Vam rado odgovoriti i na
-            poruke.
+            {ITEM7_TEXT}
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="item-8">
           <AccordionHeader icon={<Plus/>}>
-            Karate zajednica
+            {ITEM8_TITLE}
           </AccordionHeader>
           <AccordionPanel>
-            Da bismo Vas na vreme obaveštavali o svemu, koristimo Viber zajednicu. Kada Vaše dete postane član kluba, Vi
-            ćete biti uključeni u zajednicu gde sekretar povremeno šalje važne poruke koje se odnose na funkcionisanje
-            kluba.
+            {ITEM8_TEXT}
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
--- karatecukarica/src/constants.jsx
+++ karatecukarica/src/constants.jsx
@@ -39,7 +39,7 @@ export const SPLIT_IMG_ALT = "Image for split component";
 export const SPLIT_TITLE = "Title";
 export const SPLIT_TEXT =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam alias nesciunt possimus, ipsum nobis eos dolorum, est adipisci numquam libero architecto esse explicabo deserunt illo! Architecto repudiandae adipisci eveniet amet pariatur vel quidem incidunt accusamus iure dolores laborum doloribus consequatur nostrum, possimus sunt quae ducimus optio quam odio non! Eligendi?";
-//Enroll
+//Enroll.jsx
 export const ENROLL_TITLE = "Upis je u toku!";
 export const ENROLL_SUBTITLE = "POSTANI ČLAN";
 export const ENROLL_TEXT = (
@@ -50,4 +50,28 @@ export const ENROLL_TEXT = (
   </>
 );
 export const ENROLL_BUTTON_TEXT = "POGLEDAJ LOKACIJE";
-
+//Rules.jsx
+export const ITEM1_TITLE = "Oprema za trening";
+export const ITEM1_TEXT = " Da bi dete počelo da vežba karate, kimono nije neophodan. Dovoljni su trenerka dugih nogavica i majica" +
+  "            kratkih rukava.";
+export const ITEM2_TITLE = "Osvežavajući napici";
+export const ITEM2_TEXT = " Jedini napitak koji može da hidrira dete je VODA i zato nije poželjno donositi sokove i slične napitke. Potrebno je da dete ima svoju PLASTIČNU flašicu, obeleženu tako da može da je prepozna."
+export const ITEM3_TITLE = "Priprema za trening i presvlačenje";
+export const ITEM3_TEXT = "Bez obzira koliko godina deca imaju, potrebno je da se uče samostalnosti i odgovornosti. To je jedan od zadataka i ciljeva treninga. Zato je važno dovesti dete na vreme (desetak minuta pre treninga) i pustiti ga da se samostalno presvuče."
+export const ITEM4_TITLE = "Higijena";
+export const ITEM4_TEXT = "Deca se uče da održavaju ličnu higijenu, ali i da vode računa o čistoći prostora koji koriste i u kojem\n" +
+  "            borave. To je naša zajednička odgovornost. U sali i svlačionici deca hodaju bosa. Iz tog razloga, samo\n" +
+  "            vežbači mogu da koriste ovaj prostor i o njemu vode računa.";
+export const ITEM5_TITLE = "Prisustvo roditelja na treningu";
+export const ITEM5_TEXT = "Roditeljima nije dozvoljen ulazak u salu. Trening je vreme Vašeg deteta i čini deo njegove privatnosti. Osim\n" +
+  "            toga, Vaše prisustvo bi remetilo detetovu pažnju i otežavalo posao trenera."
+export const ITEM6_TITLE = "Komunikacija trenera i roditelja";
+export const ITEM6_TEXT = "Molimo Vas za strpljenje. U vreme treninga, trener ne može komunicirati sa roditeljima, ali to može učiniti\n" +
+  "            pre ili nakon treninga."
+export const ITEM7_TITLE = "Komunikacija sa sekretarom kluba";
+export const ITEM7_TEXT = "Sekretar kluba dostupan Vam je pre ili posle treninga u prostorijama kluba, ali će Vam rado odgovoriti i na\n" +
+  "            poruke."
+export const ITEM8_TITLE = "Karate zajednica";
+export const ITEM8_TEXT = " Da bismo Vas na vreme obaveštavali o svemu, koristimo Viber zajednicu. Kada Vaše dete postane član kluba, Vi\n" +
+  "            ćete biti uključeni u zajednicu gde sekretar povremeno šalje važne poruke koje se odnose na funkcionisanje\n" +
+  "            kluba.";
