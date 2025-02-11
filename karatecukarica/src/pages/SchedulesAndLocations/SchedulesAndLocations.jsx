688d04087698aea896a4dc0d0cc03ae464d9e900
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-02-11 12:56:38 +0100

    done

--- karatecukarica/src/pages/SchedulesAndLocations/SchedulesAndLocations.jsx
+++ karatecukarica/src/pages/SchedulesAndLocations/SchedulesAndLocations.jsx
@@ -1,5 +1,10 @@
+import HerobannerV2 from "../../components/HerobannerV2/HerobannerV2";
+import {LOCATIONS_IMG_ALT, LOCATIONS_TITLE1, LOCATIONS_TITLE2} from "../../constants";
+import locations from "../../assets/locations_img.jpg";
+
 const SchedulesAndLocations = () => {
-  return <div>Schedules&Locations</div>;
+  return <div><HerobannerV2 image={locations} alt={LOCATIONS_IMG_ALT} title1={LOCATIONS_TITLE1}
+                            title2={LOCATIONS_TITLE2}/></div>;
 };
 
 export default SchedulesAndLocations;
--- karatecukarica/src/pages/Terms/Terms.jsx
+++ karatecukarica/src/pages/Terms/Terms.jsx
@@ -1,7 +1,12 @@
-import Rules from "../../components/Rules/Rules.jsx";
+import HerobannerV2 from "../../components/HerobannerV2/HerobannerV2";
+import terms from "../../assets/terms_img.jpg";
+import {TERMS_IMG_ALT, TERMS_TITLE1, TERMS_TITLE2} from "../../constants";
 
 const Terms = () => {
-  return <div style={{margin: "50px 0"}}><Rules/></div>;
+  return <div>
+    <HerobannerV2 image={terms} alt={TERMS_IMG_ALT} title1={TERMS_TITLE1} title2={TERMS_TITLE2}/>
+    {/*<Rules/>*/}
+  </div>;
 };
 
 export default Terms;
