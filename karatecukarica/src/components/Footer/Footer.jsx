5cf6b75a5e59a1e6ae9eeb273f51b7a789763071
lenalonc <115535945+lenalonc@users.noreply.github.com>
2025-02-11 20:19:09 +0100

    11 footer component (#45)
    * x
    
    * Footer adjustments
    
    * Footer fine tuning
    
    * Footer adjustments
    
    * quick fix

---
+++ karatecukarica/src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import "./Footer.scss";
+import { FOOTER_TITLE, FOOTER_TXT1, FOOTER_TXT2 } from "../../constants";
+import FormComponent from "../FormComponent/FormComponent";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-contact">
+          <div className="footer-title">{FOOTER_TITLE}</div>
+          <div>
+            {FOOTER_TXT1}
+            {FOOTER_TXT2}
+          </div>
+        </div>
+        <div className="footer-form">
+          <FormComponent />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
