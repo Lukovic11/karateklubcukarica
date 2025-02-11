551a26ffab4c655aaf1f5f14f8c874570d67926c
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-02-11 13:06:46 +0100

    fix

--- karatecukarica/src/pages/Terms/Terms.jsx
+++ karatecukarica/src/pages/Terms/Terms.jsx
@@ -1,11 +1,12 @@
 import HerobannerV2 from "../../components/HerobannerV2/HerobannerV2";
 import terms from "../../assets/terms_img.jpg";
 import {TERMS_IMG_ALT, TERMS_TITLE1, TERMS_TITLE2} from "../../constants";
+import Rules from "../../components/Rules/Rules";
 
 const Terms = () => {
   return <div>
     <HerobannerV2 image={terms} alt={TERMS_IMG_ALT} title1={TERMS_TITLE1} title2={TERMS_TITLE2}/>
-    {/*<Rules/>*/}
+    <Rules/>
   </div>;
 };
 
