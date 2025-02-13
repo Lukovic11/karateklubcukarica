edd345bf25176526ef87d10b20c7dd53c5ef34dc
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-02-13 16:40:52 +0100

    maps and fixes

--- karatecukarica/src/pages/Root.jsx
+++ karatecukarica/src/pages/Root.jsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 const Root = () => {
+  const location = useLocation();
+
+  // Check if the route is not defined (404)
+  const hideFooterRoutes = ["/404"]; // Add other paths if needed
+  const shouldShowFooter = !hideFooterRoutes.includes(location.pathname);
+
   return (
     <div className="container">
-      <Header />
+      <Header/>
       <main>
-        <Outlet />
+        <Outlet/>
       </main>
-      <Footer />
+      {shouldShowFooter && <Footer/>}
     </div>
   );
 };
