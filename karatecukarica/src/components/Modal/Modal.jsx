e5dc3dabc33e6b2d5150a55fd927c4b1b615b48d
Lena Loncarevic <l.loncarevic-int@levi9.com>
2025-02-24 20:09:57 +0100

    Rules page adjustment

--- karatecukarica/src/components/Modal/Modal.jsx
+++ karatecukarica/src/components/Modal/Modal.jsx
@@ -1,21 +1,13 @@
 import "./Modal.scss"
-import Button from "../../components/Button/Button.jsx";
-import {
-    BUTTON_VARIATION_TRANSPARENT, RULES_BUTTON_TEXT1, RULES_BUTTON_TEXT2,
-    RULES_SUBTITLE1, RULES_SUBTITLE2, RULES_TEXT,
-    RULES_TITLE, SCHEDULES_AND_LOCATIONS_URL
-
-} from "../../constants.jsx";
+import {MODAL_BUTTON_CLOSE} from "../../constants.jsx";
 import PropTypes from "prop-types";
 import closeIcon from "../../assets/x-symbol.svg";
 
-
 const Modal = ({title, text, onClose}) => {
-
     return (
-        <div className="modal-container"  onClick={onClose}>
+        <div className="modal-container" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <img src={closeIcon} alt="Close" className="close-button" onClick={onClose}/>
+                <img src={closeIcon} alt={MODAL_BUTTON_CLOSE} className="close-button" onClick={onClose}/>
                 <h2>{title}</h2>
                 {text}
             </div>
