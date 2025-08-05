5453885ec9dd7b839237eb7523d12dd183b19f92
Ivana Lukovic <115374310+Lukovic11@users.noreply.github.com>
2025-08-05 18:37:12 +0200

    byeee (#64)

--- karatecukarica/src/components/Cards/Cards.jsx
+++ karatecukarica/src/components/Cards/Cards.jsx
@@ -4,13 +4,9 @@ import nenad from "../../assets/staff/nenad.jpg";
 import miki from "../../assets/staff/miki.jpg";
 import vesna from "../../assets/staff/vesna.jpg";
 import dusan from "../../assets/staff/dusan.jpg";
-import dunja from "../../assets/staff/dunja.jpg";
 import kaja from "../../assets/staff/kaja.jpg";
 import jasmina from "../../assets/staff/jasmina.jpg";
 import {
-  DUNJA,
-  DUNJA_TEXT,
-  DUNJA_TITLE,
   DUSAN,
   DUSAN_TEXT,
   DUSAN_TITLE,
@@ -41,7 +37,6 @@ const Homepage = () => {
       <HoverCard name={VESNA} title={VESNA_TITLE} text={VESNA_TEXT} image={vesna}/>
       <HoverCard name={MIROSLAV} title={MIROSLAV_TITLE} text={MIROSLAV_TEXT} image={miki}/>
       <HoverCard name={DUSAN} title={DUSAN_TITLE} text={DUSAN_TEXT} image={dusan}/>
-      <HoverCard name={DUNJA} title={DUNJA_TITLE} text={DUNJA_TEXT} image={dunja}/>
       <HoverCard name={KATARINA} title={KATARINA_TITLE} text={KATARINA_TEXT} image={kaja}/>
       <HoverCard name={JASMINA} title={JASMINA_TITLE} text={JASMINA_TEXT} image={jasmina}/>
     </div>
